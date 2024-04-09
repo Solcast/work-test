87c813e3b8561e7b3b9752d0809e51420f7d1084
alex-solcast <alex@solcast.com.au>
2024-04-09 13:07:40 +1000

    Added a distance function for convenience

--- NearestPoints/Program.cs
+++ NearestPoints/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+/// Calculate the linear distance between a point and a target t
+static float distance(float x, float y, float xt, float yt)
+{
+    var dx = x-xt; //delta between point and target
+    var dy = y-yt;
+    return (float)Math.Sqrt(dx*dx + dy*dy);
+}
+
+
 Console.WriteLine("Hello, World!");
